2f7b92f739918f42ba25783b1a340f9b886f9dd9
Turazzi <analivia.turazzi@gmail.com>
2025-04-29 19:34:42 -0300

    feat: Implemented basic payment saving functionally

--- backend/src/main/java/br/ifsp/demo/service/PagamentoService.java
+++ backend/src/main/java/br/ifsp/demo/service/PagamentoService.java
@@ -1,4 +1,24 @@
 package br.ifsp.demo.service;
 
+import br.ifsp.demo.model.Pagamento;
+import br.ifsp.demo.model.Veiculo;
+import br.ifsp.demo.repository.PagamentoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class PagamentoService {
+
+    public final PagamentoRepository pagamentoRepository;
+
+    @Autowired
+
+    public PagamentoService(PagamentoRepository pagamentoRepository) {
+        this.pagamentoRepository = pagamentoRepository;
+    }
+
+    public void salvarPagamento(Veiculo veiculo) {
+        Pagamento pagamento = new Pagamento(veiculo);
+        pagamentoRepository.save(pagamento);
+    }
 }
