258245fa87c982e38676cd518127c725503863db
Alan-VSouza <alansouzeesi@gmail.com>
2025-05-01 16:21:34 -0300

    feat: Add update method in VeiculoController for successful vehicle update and changes in VeiculoService

--- backend/src/main/java/br/ifsp/demo/controller/VeiculoController.java
+++ backend/src/main/java/br/ifsp/demo/controller/VeiculoController.java
@@ -54,4 +54,39 @@ public class VeiculoController {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
         }
     }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> atualizarVeiculo(@PathVariable Long id, @RequestBody Veiculo veiculo) {
+        if (veiculo.getPlaca() == null || veiculo.getPlaca().trim().isEmpty()) {
+            ErrorResponse errorResponse = new ErrorResponse("Placa não pode ser vazia");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+        }
+
+        if (veiculo.getModelo() == null || veiculo.getModelo().trim().isEmpty()) {
+            ErrorResponse errorResponse = new ErrorResponse("Modelo não pode ser vazio");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+        }
+
+        if (veiculo.getCor() == null || veiculo.getCor().trim().isEmpty()) {
+            ErrorResponse errorResponse = new ErrorResponse("Cor não pode ser vazia");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+        }
+
+        if (veiculo.getHoraEntrada() == null) {
+            ErrorResponse errorResponse = new ErrorResponse("Hora de entrada não pode ser nula");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+        }
+
+        Veiculo veiculoExistente = veiculoService.atualizarVeiculo(id, veiculo.getPlaca(),
+                veiculo.getTipoVeiculo(), veiculo.getModelo(), veiculo.getCor());
+
+        if (veiculoExistente == null) {
+            ErrorResponse errorResponse = new ErrorResponse("Veículo não encontrado");
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(veiculoExistente);
+    }
+
+
 }
