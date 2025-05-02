9d8e212f56b501037295489b01b8f0df88f5509e
Turazzi <analivia.turazzi@gmail.com>
2025-05-02 15:59:26 -0300

    test: Adds funcionality test for request to delete payment and refactors vehicle creation for testing

--- backend/src/test/java/br/ifsp/demo/controller/PagamentoControllerTest.java
+++ backend/src/test/java/br/ifsp/demo/controller/PagamentoControllerTest.java
@@ -15,11 +15,12 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doNothing;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -35,6 +36,8 @@ class PagamentoControllerTest {
     private MockMvc mockMvc;
     private ObjectMapper objectMapper;
 
+    private Veiculo veiculo;
+
     @BeforeEach
     void setUp() {
 
@@ -46,6 +49,14 @@ class PagamentoControllerTest {
         mockMvc = MockMvcBuilders.standaloneSetup(pagamentoController)
                 .defaultRequest(get("/").accept(MediaType.APPLICATION_JSON))
                 .build();
+
+        veiculo = new Veiculo();
+        veiculo.setId(1L);
+        veiculo.setPlaca("QBC-2994");
+        veiculo.setTipoVeiculo("carro");
+        veiculo.setModelo("Escort Hobby");
+        veiculo.setCor("prata");
+        veiculo.setHoraEntrada(LocalDateTime.now().minusHours(6));
 
     }
 
@@ -59,14 +70,6 @@ class PagamentoControllerTest {
         @DisplayName("Deve criar o pagamento com sucesso")
         void deveCriarPagamentoComSucesso() throws Exception {
 
-            Veiculo veiculo = new Veiculo();
-            veiculo.setId(1L);
-            veiculo.setPlaca("QBC-2994");
-            veiculo.setTipoVeiculo("carro");
-            veiculo.setModelo("Escort Hobby");
-            veiculo.setCor("prata");
-            veiculo.setHoraEntrada(LocalDateTime.now().minusHours(6));
-
             Pagamento pagamento = new Pagamento(veiculo);
             pagamento.setValor(35.0);
 
@@ -77,6 +80,23 @@ class PagamentoControllerTest {
                             .content(objectMapper.writeValueAsString(pagamento)))
                     .andExpect(status().isCreated())
                     .andExpect(jsonPath("$.valor").value(35.0));
+        }
+
+        @Test
+        @Tag("TDD")
+        @Tag("UnitTest")
+        @DisplayName("Deve deletar o pagamento com sucesso")
+        void deveDeletarPagamentoComSucesso() throws Exception {
+
+            Pagamento pagamento = new Pagamento(veiculo);
+            pagamento.setUuid(UUID.randomUUID());
+            pagamento.setValor(35.0);
+
+            when(pagamentoService.buscarPorId(pagamento.getUuid())).thenReturn(pagamento);
+            doNothing().when(pagamentoService).deletarPagamento(pagamento);
+
+            mockMvc.perform(delete("/api/pagamento/{id}", pagamento.getUuid()))
+                    .andExpect(status().isNoContent());
         }
 
     }
