bc1c4d243263c6229372fa7e6161aaf8505abde6
Turazzi <analivia.turazzi@gmail.com>
2025-05-02 20:13:16 -0300

    feat: Add payment search by date in repository

--- backend/src/main/java/br/ifsp/demo/repository/PagamentoRepository.java
+++ backend/src/main/java/br/ifsp/demo/repository/PagamentoRepository.java
@@ -5,8 +5,11 @@ import br.ifsp.demo.model.Pagamento;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface PagamentoRepository extends JpaRepository<Pagamento, UUID> {
+    List<Pagamento> findByHoraSaidaBetween(LocalDateTime inicioDoDia, LocalDateTime fimDoDia);
 }
