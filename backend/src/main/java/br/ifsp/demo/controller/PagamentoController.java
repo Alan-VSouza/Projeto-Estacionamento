8577a2a2510499a22aa46d5a0dee476c473bfd85
Turazzi <analivia.turazzi@gmail.com>
2025-05-01 23:02:46 -0300

    feat: Add implementation of criarPagamento  in PagamentoController

--- backend/src/main/java/br/ifsp/demo/controller/PagamentoController.java
+++ backend/src/main/java/br/ifsp/demo/controller/PagamentoController.java
@@ -1,4 +1,55 @@
 package br.ifsp.demo.controller;
 
+import br.ifsp.demo.dto.ErrorResponse;
+import br.ifsp.demo.model.Pagamento;
+import br.ifsp.demo.service.PagamentoService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/api/pagamento")
 public class PagamentoController {
+
+    private final PagamentoService pagamentoService;
+
+    public PagamentoController(PagamentoService pagamentoService) {
+        this.pagamentoService = pagamentoService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> criarPagamento(@RequestBody Pagamento pagamento) {
+        if (pagamento.getVeiculo() == null) {
+            ErrorResponse errorResponse = new ErrorResponse("Veículo não pode ser nulo");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+        }
+
+        if(pagamento.getHoraSaida() == null) {
+            ErrorResponse errorResponse = new ErrorResponse("Hora de saída não pode ser nula");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+        }
+
+        if(pagamento.getHoraEntrada() == null) {
+            ErrorResponse errorResponse = new ErrorResponse("Hora de entrada não pode ser nula");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+        }
+
+        if(pagamento.getHoraEntrada().isAfter(pagamento.getHoraSaida())) {
+            ErrorResponse errorResponse = new ErrorResponse("Hora de saída não pode estar antes da hora de entrada");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+        }
+
+        try {
+            pagamentoService.salvarPagamento(pagamento);
+            return ResponseEntity.status(HttpStatus.CREATED).body(pagamento);
+        } catch (IllegalArgumentException e) {
+            ErrorResponse errorResponse = new ErrorResponse(e.getMessage());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+        }
+    }
+
 }
