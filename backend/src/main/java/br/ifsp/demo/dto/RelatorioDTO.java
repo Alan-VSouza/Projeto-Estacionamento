08742de9f8a0fcf29b898c63f9c1e101e870c453
junior220503 <fabianomarochauga@yahoo.com>
2025-05-03 02:58:36 -0300

    feat: Generate performance relatory controller implemented

--- backend/src/main/java/br/ifsp/demo/dto/RelatorioDTO.java
+++ backend/src/main/java/br/ifsp/demo/dto/RelatorioDTO.java
@@ -1,4 +1,18 @@
 package br.ifsp.demo.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class RelatorioDTO {
+
+    private int quantidadeVeiculos;
+    private double tempoMedioEstadia;
+    private double receitaTotal;
+    private double ocupacaoMedia;
 }
--- backend/src/main/java/br/ifsp/demo/service/RelatorioService.java
+++ backend/src/main/java/br/ifsp/demo/service/RelatorioService.java
@@ -1,7 +1,12 @@
 package br.ifsp.demo.service;
 
+import br.ifsp.demo.dto.RelatorioDTO;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RelatorioService {
+
+    public RelatorioDTO gerarRelatorioDesempenho() {
+        return new RelatorioDTO(100, 2.5, 3500.00, 75.0);
+    }
 }
