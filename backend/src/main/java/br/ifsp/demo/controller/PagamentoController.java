92269bfe3a96c4ae30af0c50366fd6fdcc902a15
Turazzi <analivia.turazzi@gmail.com>
2025-05-04 19:37:17 -0300

    refactor: Refactoring payment controller fixing small rest defects

--- backend/src/main/java/br/ifsp/demo/controller/PagamentoController.java
+++ backend/src/main/java/br/ifsp/demo/controller/PagamentoController.java
@@ -23,72 +23,53 @@ public class PagamentoController {
 
     @PostMapping
     public ResponseEntity<Object> criarPagamento(@RequestBody Pagamento pagamento) {
-
         if (veiculoService.buscarPorPlaca(pagamento.getPlaca()).isEmpty()) {
-            ErrorResponse errorResponse = new ErrorResponse("Veículo precisa estar registrado no banco");
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse("Veículo precisa estar registrado no banco"));
         }
 
         if(pagamento.getHoraSaida() == null) {
-            ErrorResponse errorResponse = new ErrorResponse("Hora de saída não pode ser nula");
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse("Hora de saída não pode ser nula"));
         }
 
         if(pagamento.getHoraEntrada() == null) {
-            ErrorResponse errorResponse = new ErrorResponse("Hora de entrada não pode ser nula");
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse("Hora de entrada não pode ser nula"));
         }
 
         if(pagamento.getHoraEntrada().isAfter(pagamento.getHoraSaida())) {
-            ErrorResponse errorResponse = new ErrorResponse("Hora de saída não pode estar antes da hora de entrada");
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse("Hora de saída não pode estar antes da hora de entrada"));
         }
 
         try {
             pagamentoService.salvarPagamento(pagamento);
             return ResponseEntity.status(HttpStatus.CREATED).body(pagamento);
-        } catch (IllegalArgumentException e) {
-            ErrorResponse errorResponse = new ErrorResponse(e.getMessage());
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse(e.getMessage()));
         }
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Object> excluirPagamento(@PathVariable UUID id) {
-
         if (id == null) {
-            ErrorResponse errorResponse = new ErrorResponse("Id do pagamento não pode ser nulo");
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse("Id do pagamento não pode ser nulo"));
         }
 
         try {
             Pagamento pagamento = pagamentoService.buscarPorId(id);
-
             if (pagamento == null) {
                 return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponse("Pagamento não encontrado"));
             }
 
             pagamentoService.deletarPagamento(pagamento);
             return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
-
         } catch (Exception e) {
-            ErrorResponse errorResponse = new ErrorResponse(e.getMessage());
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse(e.getMessage()));
         }
     }
 
-
     @PutMapping("/{id}")
     public ResponseEntity<Object> atualizarPagamento(@PathVariable UUID id, @RequestBody Pagamento pagamentoAtualizado) {
-
-        if(id == null) {
-            ErrorResponse errorResponse = new ErrorResponse("Id do pagamento não pode ser nulo");
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
-        }
-
-        if(pagamentoAtualizado == null) {
-            ErrorResponse errorResponse = new ErrorResponse("Pagamento atualizado não pode ser nulo");
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+        if(id == null || pagamentoAtualizado == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse("Id ou pagamento não pode ser nulo"));
         }
 
         try {
@@ -100,28 +81,22 @@ public class PagamentoController {
                     pagamentoAtualizado.getValor()
             );
             return ResponseEntity.ok(pagamento);
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse(e.getMessage()));
         }
-
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Object> buscarPagamentoPorId(@PathVariable UUID id) {
-
         if (id == null) {
-            ErrorResponse errorResponse = new ErrorResponse("Id do pagamento não pode ser nulo");
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse("Id do pagamento não pode ser nulo"));
         }
 
         try {
             Pagamento pagamento = pagamentoService.buscarPorId(id);
-
             if (pagamento == null) {
                 return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponse("Pagamento não encontrado"));
             }
-
             return ResponseEntity.ok(pagamento);
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponse(e.getMessage()));
