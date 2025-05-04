560808306e3a014aae598142c3aea34cbb730304
Alan-VSouza <alansouzeesi@gmail.com>
2025-05-04 02:14:13 -0300

    test: new test to register exit hour and register payment

--- backend/src/test/java/br/ifsp/demo/service/EstacionamentoServiceTest.java
+++ backend/src/test/java/br/ifsp/demo/service/EstacionamentoServiceTest.java
@@ -1,15 +1,18 @@
 package br.ifsp.demo.service;
 
 import br.ifsp.demo.model.Estacionamento;
+import br.ifsp.demo.model.Pagamento;
 import br.ifsp.demo.model.RegistroEntrada;
 import br.ifsp.demo.model.Veiculo;
 import br.ifsp.demo.repository.EstacionamentoRepository;
+import br.ifsp.demo.repository.PagamentoRepository;
 import br.ifsp.demo.repository.RegistroEntradaRepository;
 import org.junit.jupiter.api.*;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -24,6 +27,12 @@ public class EstacionamentoServiceTest {
     private RegistroEntradaRepository registroEntradaRepository;
 
     @Mock
+    private PagamentoService pagamentoService;
+
+    @Mock
+    private PagamentoRepository pagamentoRepository;
+
+    @Mock
     private VeiculoService veiculoService;
 
 
@@ -35,6 +44,7 @@ public class EstacionamentoServiceTest {
     private RegistroEntrada registroEntrada;
 
     private static final String PLACA_VEICULO = "ABC1234";
+    private static final String PLACA = PLACA_VEICULO;
 
     @BeforeEach
     void setup() {
@@ -53,6 +63,7 @@ public class EstacionamentoServiceTest {
 
         registroEntrada = new RegistroEntrada();
         registroEntrada.setVeiculo(veiculo);
+        registroEntrada.setHoraEntrada(LocalDateTime.now().minusHours(2));
     }
 
     @Test
@@ -107,5 +118,31 @@ public class EstacionamentoServiceTest {
 
         assertEquals("Veículo não registrado no estacionamento", exception.getMessage());
     }
+
+    @Test
+    @Tag("TDD")
+    @DisplayName("Registrar saída com sucesso: gera pagamento e remove entrada")
+    public void registrarSaida_comSucesso_salvaPagamentoERemoveEntrada() {
+        when(veiculoService.buscarPorPlaca(PLACA_VEICULO))
+                .thenReturn(Optional.of(veiculo));
+        when(registroEntradaRepository.findByVeiculo(veiculo))
+                .thenReturn(Optional.of(registroEntrada));
+
+        doNothing().when(pagamentoService).salvarPagamento(any(Pagamento.class));
+
+        boolean resultado = estacionamentoService.registrarSaida(veiculo);
+
+        assertTrue(resultado);
+
+        verify(registroEntradaRepository, times(1))
+                .delete(registroEntrada);
+
+        verify(pagamentoService).salvarPagamento(argThat(p ->
+                PLACA_VEICULO.equals(p.getPlaca()) &&
+                        p.getHoraEntrada().equals(registroEntrada.getHoraEntrada()) &&
+                        p.getHoraSaida() != null
+        ));
+    }
+
 
 }
