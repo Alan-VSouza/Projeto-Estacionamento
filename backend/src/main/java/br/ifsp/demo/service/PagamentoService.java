c9d212e3eba5c4e2ff804e0fd0f959e39aa7a5ee
Alan-VSouza <alansouzeesi@gmail.com>
2025-05-05 00:36:09 -0300

    fix: Update import about TempoPermanencia

--- backend/src/main/java/br/ifsp/demo/service/PagamentoService.java
+++ backend/src/main/java/br/ifsp/demo/service/PagamentoService.java
@@ -1,7 +1,7 @@
 package br.ifsp.demo.service;
 
 import br.ifsp.demo.model.Pagamento;
-import br.ifsp.demo.model.TempoPermanencia;
+import br.ifsp.demo.components.TempoPermanencia;
 import br.ifsp.demo.model.Veiculo;
 import br.ifsp.demo.exception.PagamentoNaoEncontradoException;
 import br.ifsp.demo.exception.VeiculoNaoEncontradoException;
