84e08f4b0fbf769416ec2881353013b22858ea4e
Alan-VSouza <alansouzeesi@gmail.com>
2025-05-05 00:30:56 -0300

    refactor: Simplify response handling and improve controller logic in RelatorioController

--- backend/src/main/java/br/ifsp/demo/controller/RelatorioController.java
+++ backend/src/main/java/br/ifsp/demo/controller/RelatorioController.java
@@ -4,7 +4,7 @@ import br.ifsp.demo.dto.HistoricoDTO;
 import br.ifsp.demo.dto.ReciboDTO;
 import br.ifsp.demo.dto.RelatorioDTO;
 import br.ifsp.demo.service.RelatorioService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,26 +15,25 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/relatorios")
+@RequiredArgsConstructor
 public class RelatorioController {
 
     private final RelatorioService relatorioService;
 
-    @Autowired
-    public RelatorioController(RelatorioService relatorioService) {
-        this.relatorioService = relatorioService;
-    }
-
     @GetMapping("/desempenho")
-    public ResponseEntity<RelatorioDTO> gerarRelatorioDesempenho(@RequestParam("data") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate data) {
-        RelatorioDTO relatorio = relatorioService.gerarRelatorioDesempenho(data);
-        return ResponseEntity.ok(relatorio);
+    public ResponseEntity<RelatorioDTO> gerarRelatorioDesempenho(@RequestParam("data")
+                                                                 @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate data) {
+        return ResponseEntity.ok(relatorioService.gerarRelatorioDesempenho(data));
     }
 
     @GetMapping("/recibo")
     public ResponseEntity<ReciboDTO> gerarRecibo(@RequestParam("placa") String placa) {
         ReciboDTO recibo = relatorioService.gerarRecibo(placa);
-        if (recibo == null) {return ResponseEntity.notFound().build();}
-        return ResponseEntity.ok(recibo);
+        if (recibo != null) {
+            return ResponseEntity.ok(recibo);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @GetMapping("/historico/{placa}")
--- backend/src/test/java/br/ifsp/demo/model/TempoPermanenciaTest.java
+++ backend/src/test/java/br/ifsp/demo/model/TempoPermanenciaTest.java
@@ -1,5 +1,6 @@
 package br.ifsp.demo.model;
 
+import br.ifsp.demo.components.TempoPermanencia;
 import br.ifsp.demo.components.ValorPermanencia;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
