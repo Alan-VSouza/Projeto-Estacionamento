11a982c5e64bb60ed40245243563fb0ccffb425f
Alan-VSouza <alansouzeesi@gmail.com>
2025-05-05 00:32:35 -0300

    fix: Add on Veiculo @JsonIgnore to prevent issues during registration

--- backend/src/main/java/br/ifsp/demo/model/Veiculo.java
+++ backend/src/main/java/br/ifsp/demo/model/Veiculo.java
@@ -1,5 +1,6 @@
 package br.ifsp.demo.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -13,6 +14,7 @@ public class Veiculo {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonIgnore
     private Long id;
 
     @Column(unique = true, nullable = false)
