339cc46f7a792107e7980a147a7c05e93aa9f49d
Turazzi <analivia.turazzi@gmail.com>
2025-05-31 00:25:10 -0300

    refactor: make Veiculo entity immutable

--- backend/src/main/java/br/ifsp/demo/components/LogSistema.java
+++ backend/src/main/java/br/ifsp/demo/components/LogSistema.java
@@ -8,4 +8,5 @@ public class LogSistema {
     public void registrarCancelamento(String placa, String motivoCancelamento) {
         System.out.println("Cancelamento do check-in do veículo " + placa + " realizado. Motivo: " + motivoCancelamento);
     }
+
 }
--- backend/src/main/java/br/ifsp/demo/model/Veiculo.java
+++ backend/src/main/java/br/ifsp/demo/model/Veiculo.java
@@ -14,9 +14,6 @@ public class Veiculo {
     private Long id;
 
     @Column(unique = true, nullable = false)
-    private LocalDateTime horaEntrada;
-
-    @Column(unique = true, nullable = false)
     private String placa;
 
     @Column(nullable = false)
@@ -28,69 +25,43 @@ public class Veiculo {
     @Column(nullable = false)
     private String cor;
 
-    public Veiculo() {}
+    protected Veiculo() {}
 
-    public Veiculo(String placa, String tipoVeiculo, String modelo, String cor, LocalDateTime horaEntrada) {
-        setPlaca(placa);
-        setTipoVeiculo(tipoVeiculo);
-        setModelo(modelo);
-        setCor(cor);
-        setHoraEntrada(horaEntrada);
+    public Veiculo(String placa, String tipoVeiculo, String modelo, String cor) {
+
+        if(placa == null || placa.trim().isEmpty())
+            throw new IllegalArgumentException("Placa não pode ser nula ou vazia");
+
+        if(tipoVeiculo == null || tipoVeiculo.trim().isEmpty())
+            throw new IllegalArgumentException("Tipo do veículo não pode ser nulo ou vazio");
+
+        if (modelo == null || modelo.trim().isEmpty())
+            throw new IllegalArgumentException("Modelo do veículo não pode ser nulo ou vazio");
+
+        if (cor == null || cor.trim().isEmpty())
+            throw new IllegalArgumentException("Cor do veículo não pode ser nula ou vazia");
+
+        this.placa = placa;
+        this.tipoVeiculo = tipoVeiculo;
+        this.modelo = modelo;
+        this.cor = cor;
+
     }
 
     public Long getId() {
         return id;
     }
-
-    public LocalDateTime getHoraEntrada() {
-        return horaEntrada;
-    }
-
-    public void setHoraEntrada(LocalDateTime horaEntrada) {
-        if (horaEntrada == null)
-            throw new IllegalArgumentException("Hora de entrada não pode ser nula");
-        if (horaEntrada.isAfter(LocalDateTime.now()))
-            throw new IllegalArgumentException("Hora de entrada não pode ser no futuro");
-        this.horaEntrada = horaEntrada;
-    }
-
     public String getPlaca() {
         return placa;
     }
-
-    public void setPlaca(String placa) {
-        if (placa == null || placa.trim().isEmpty())
-            throw new IllegalArgumentException("Placa não pode ser nula ou vazia");
-        this.placa = placa;
-    }
-
     public String getTipoVeiculo() {
         return tipoVeiculo;
     }
-
-    public void setTipoVeiculo(String tipoVeiculo) {
-        if (tipoVeiculo == null || tipoVeiculo.trim().isEmpty())
-            throw new IllegalArgumentException("Tipo do veículo não pode ser nulo ou vazio");
-        this.tipoVeiculo = tipoVeiculo;
-    }
-
     public String getModelo() {
         return modelo;
     }
-
-    public void setModelo(String modelo) {
-        if (modelo == null || modelo.trim().isEmpty())
-            throw new IllegalArgumentException("Modelo não pode ser nulo ou vazio");
-        this.modelo = modelo;
-    }
-
     public String getCor() {
         return cor;
     }
 
-    public void setCor(String cor) {
-        if (cor == null || cor.trim().isEmpty())
-            throw new IllegalArgumentException("Cor não pode ser nula ou vazia");
-        this.cor = cor;
-    }
 }
