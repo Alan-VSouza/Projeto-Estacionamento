d48a8918e4e691aff12bf0723085995ba05cc5b2
Turazzi <analivia.turazzi@gmail.com>
2025-06-02 05:37:24 -0300

    test(model): Add tests for payment second constructor

--- backend/src/test/java/br/ifsp/demo/model/PagamentoTest.java
+++ backend/src/test/java/br/ifsp/demo/model/PagamentoTest.java
@@ -108,5 +108,92 @@ class PagamentoTest {
             }
 
         }
+
+        @Nested
+        @DisplayName("Teste do segundo construtor")
+        class TesteDoSegundoConstrutor {
+
+            @Test
+            @Tag("UnitTest")
+            @Tag("Mutation")
+            @DisplayName("Deve lançar exceção quando o registro de entrada for nulo")
+            void deveLancarUmaExcecaoQuandoORegistroDeEntradaForNulo() {
+
+                IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+                    Pagamento pagamento = new Pagamento(null, entrada, saida, calculadoraDeTarifa);
+                });
+
+                assertThat(exception.getMessage()).isEqualTo("Registro de entrada não pode ser nulo");
+            }
+
+            @Test
+            @Tag("UnitTest")
+            @Tag("Mutation")
+            @DisplayName("Deve lançar exceção quando o hora de entrada for nulo")
+            void deveLancarUmaExcecaoQuandoORHoraDeSaidaForNulo() {
+
+                IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+                    Pagamento pagamento = new Pagamento(registroEntrada, null,saida, calculadoraDeTarifa);
+                });
+
+                assertThat(exception.getMessage()).isEqualTo("Hora de entrada não pode ser nula");
+            }
+
+            @Test
+            @Tag("UnitTest")
+            @Tag("Mutation")
+            @DisplayName("Deve lançar exceção quando o hora de saida for nulo")
+            void deveLancarUmaExcecaoQuandoORHoraDeEntradaForNulo() {
+
+                IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+                    Pagamento pagamento = new Pagamento(registroEntrada, entrada,null, calculadoraDeTarifa);
+                });
+
+                assertThat(exception.getMessage()).isEqualTo("Hora de saída não pode ser nula");
+            }
+
+            @Test
+            @Tag("UnitTest")
+            @Tag("Mutation")
+            @DisplayName("Deve lançar exceção quando o tarifa for nulo")
+            void deveLancarUmaExcecaoQuandoORTarifaForNulo() {
+
+                IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+                    Pagamento pagamento = new Pagamento(registroEntrada, entrada, saida, null);
+                });
+
+                assertThat(exception.getMessage()).isEqualTo("Tarifa não pode ser nula");
+            }
+
+            @Test
+            @Tag("UnitTest")
+            @Tag("Mutation")
+            @DisplayName("Deve lançar exceção quando hora de saída for anterior à de entrada")
+            void deveLancarExcecaoParaSaidaInvalida() {
+                LocalDateTime saidaInvalida = entrada.minusHours(1);
+
+                IllegalArgumentException excecao = assertThrows(IllegalArgumentException.class, () -> {
+                    new Pagamento(registroEntrada, entrada, saidaInvalida, calculadoraDeTarifa);
+                });
+
+                assertThat(excecao.getMessage()).isEqualTo("Hora de saída não pode ser antes da hora de entrada");
+            }
+
+            @Test
+            @Tag("UnitTest")
+            @Tag("Mutation")
+            @DisplayName("Deve lançar exceção quando o valor da tarifa for negativo")
+            void deveLancarExcecaoQuandoValorForNegativo() {
+                String mensagemEsperada = "Valor da tarifa não pode ser negativo";
+                when(calculadoraDeTarifa.calcularValor(any(LocalDateTime.class), any(LocalDateTime.class)))
+                        .thenReturn(-10.0);
+
+                IllegalArgumentException excecao = assertThrows(IllegalArgumentException.class, () -> {
+                    new Pagamento(registroEntrada, entrada, saida, calculadoraDeTarifa);
+                });
+
+                assertThat(excecao.getMessage()).isEqualTo(mensagemEsperada);
+            }
+        }
     }
 }
